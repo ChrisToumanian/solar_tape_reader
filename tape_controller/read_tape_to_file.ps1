345e773203268a81684f140e8dc893daa62f01df
Christopher Toumanian <cctoumanian@gmail.com>
2023-01-29 18:04:55 -0800

    Changed read tape dd block size to 1k, added tapecopy command

--- tape_controller/read_tape_to_file.ps1
+++ tape_controller/read_tape_to_file.ps1
@@ -3,4 +3,4 @@
 
 $output_name=$args[0]
 write-host "Reading TAPE0 to $output_name at current position"
-.\dd\dd.exe if=\\.\TAPE0 of=tape_data\$output_name --progress
+.\dd\dd.exe if=\\.\TAPE0 bs=1k of=tape_data\$output_name --progress
